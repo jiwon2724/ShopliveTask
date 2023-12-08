c2cf662cfa9f2c3ed154d0cd2b07c5ccd8828770
jiwon2724 <stopone3119@naver.com>
2023-12-08 17:58:41 +0900

    [UI] : TabLayout 구현 완료

--- feature/marvel/src/main/java/jiwondev/feature/marvel/MarvelActivity.kt
+++ feature/marvel/src/main/java/jiwondev/feature/marvel/MarvelActivity.kt
@@ -2,10 +2,13 @@ package jiwondev.feature.marvel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jiwondev.feature.marvel.databinding.ActivityMarvelBinding
 
 class MarvelActivity : AppCompatActivity() {
+    private val binding: ActivityMarvelBinding by lazy { ActivityMarvelBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_marvel)
+        setContentView(binding.root)
     }
 }
