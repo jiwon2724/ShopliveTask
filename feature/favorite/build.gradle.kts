f353db5eaf6177e1a6e823283139460620ad854e
정지원 <stopone3119@gmail.com>
2023-12-08 20:47:34 +0900

    [FEATURE] #7 BaseFragment 구현 (#8)
    * [CHORE] : core 모듈 생성
    
    * [FEATURE] : BaseFragment 구현
    
    * [CHORE] : BaseFragment 적용

--- feature/favorite/build.gradle.kts
+++ feature/favorite/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 android {
     namespace = "jiwondev.feature.favorite"
     compileSdk = 33
+    viewBinding { enable = true  }
 
     buildTypes {
         release {
@@ -32,4 +33,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation(project(":core:base"))
 }
