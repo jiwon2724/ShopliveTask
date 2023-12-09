b1f0c49688ecb8fd745f0c0307819929cfe3ff4b
jiwon2724 <stopone3119@naver.com>
2023-12-09 21:29:39 +0900

    [CHORE] : Retrofit 어노테이션 추가

--- data/src/main/java/jiwondev/data/api/MarvelCharacterApi.kt
+++ data/src/main/java/jiwondev/data/api/MarvelCharacterApi.kt
@@ -4,15 +4,16 @@ import jiwondev.data.BuildConfig
 import jiwondev.data.common.Constant
 import jiwondev.data.model.CharacterResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface MarvelCharacterApi {
     @GET("characters")
     suspend fun getCharacterResponse(
-        ts: String = Constant.timeStamp,
-        hash: String = Constant.getHash(),
-        apiKey: String = BuildConfig.PUBLIC_KEY,
-        nameStartsWith: String,
-        limit: Int = 10,
-        offset: Int = 10
+        @Query("ts") ts: String = Constant.timeStamp,
+        @Query("hash") hash: String = Constant.getHash(),
+        @Query("apiKey") apiKey: String = BuildConfig.PUBLIC_KEY,
+        @Query("nameStartsWith") nameStartsWith: String,
+        @Query("limit") limit: Int = 10,
+        @Query("offset") offset: Int = 10
     ): CharacterResponse
 }
