618bce637993c037373e4700dfadf3a437ac461e
jiwon2724 <stopone3119@naver.com>
2023-12-09 16:24:26 +0900

    [CHORE] : model 패키지 생성 및 data class 병합

--- data/src/main/java/jiwondev/data/response/Data.kt
+++
@@ -1,9 +0,0 @@
-package jiwondev.data.response
-
-data class Data(
-    val count: Int,
-    val limit: Int,
-    val offset: Int,
-    val results: List<Result>,
-    val total: Int
-)
--- data/src/main/java/jiwondev/data/response/Url.kt
+++
@@ -1,6 +0,0 @@
-package jiwondev.data.response
-
-data class Url(
-    val type: String,
-    val url: String
-)
