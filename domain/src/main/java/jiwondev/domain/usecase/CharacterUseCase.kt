72ed73180d7b2d42b96d74511b302cc5f9c48880
jiwon2724 <stopone3119@naver.com>
2023-12-09 23:00:02 +0900

    [BASE] : Result sealed class 구현 및 적용

--- domain/src/main/java/jiwondev/domain/usecase/CharacterUseCase.kt
+++ domain/src/main/java/jiwondev/domain/usecase/CharacterUseCase.kt
@@ -1,11 +1,12 @@
 package jiwondev.domain.usecase
 
+import jiwondev.domain.Result
 import jiwondev.domain.model.Character
 import jiwondev.domain.repository.CharacterRepository
 import javax.inject.Inject
 
 class CharacterUseCase @Inject constructor(private val repository: CharacterRepository) {
-    suspend fun getCharacter(): Character {
-        return repository.getCharacter()
+    suspend fun getCharacter(): Result<Character> {
+        return repository.getCharacter("man")
     }
 }
