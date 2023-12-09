840ab1a3f8bb97d6e39850f812500ecc99b5b7ef
jiwon2724 <stopone3119@naver.com>
2023-12-09 23:22:09 +0900

    [FEATURE] : ViewModel <-> UseCase 연동 완료

--- feature/search/src/main/java/jiwndev/feature/search/SearchFragment.kt
+++ feature/search/src/main/java/jiwndev/feature/search/SearchFragment.kt
@@ -2,14 +2,27 @@ package jiwndev.feature.search
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import dagger.hilt.android.AndroidEntryPoint
 import jiwndev.feature.search.databinding.FragmentSearchBinding
 import jiwondev.core.base.BaseFragment
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class SearchFragment : BaseFragment<FragmentSearchBinding>() {
+    private val searchViewModel: SearchViewModel by viewModels()
     override fun bindingFactory(inflater: LayoutInflater, parent: ViewGroup?): FragmentSearchBinding {
         return FragmentSearchBinding.inflate(inflater, parent, false)
     }
 
-    override fun initViews() { }
+    override fun initViews() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                searchViewModel.apiTest()
+            }
+        }
+    }
 }
