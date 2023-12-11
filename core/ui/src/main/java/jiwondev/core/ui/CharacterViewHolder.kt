03cdcdff8284daf571351b080beaa66d00f52bd0
jiwon2724 <stopone3119@naver.com>
2023-12-11 16:50:38 +0900

    [FEATURE] : CharacterViewHolder 구현

---
+++ core/ui/src/main/java/jiwondev/core/ui/CharacterViewHolder.kt
@@ -0,0 +1,16 @@
+package jiwondev.core.ui
+
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import coil.load
+import jiwondev.core.ui.databinding.ItemCharacterBinding
+import jiwondev.domain.model.CharacterInfo
+
+class CharacterViewHolder(private val binding: ItemCharacterBinding) : ViewHolder(binding.root) {
+    fun bind(characterInfo: CharacterInfo) {
+        binding.apply {
+            ivThumbnail.load(characterInfo.thumbnail)
+            tvName.text = characterInfo.name
+            tvDescription.text = characterInfo.description
+        }
+    }
+}
