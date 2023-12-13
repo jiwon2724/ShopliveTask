c0e4605f925e6d07d7ac6b67e1e21b0432f6b1ad
jiwon2724 <stopone3119@naver.com>
2023-12-14 01:02:35 +0900

    [CHORE] : 버전 카탈로그 적용

--- feature/search/build.gradle.kts
+++ feature/search/build.gradle.kts
@@ -36,15 +36,17 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation(libs.google.material)
+    implementation(libs.androidx.core)
     implementation(libs.androidx.constraintlayout)
+
+    implementation(libs.androidx.fragemnt.ktx)
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
-    implementation(libs.hilt.android)
-    kapt(libs.hilt.compiler)
 
     implementation(project(":core:base"))
     implementation(project(":core:ui"))
